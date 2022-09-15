e465ee47a29381918dd050ad630fa66cf774fc19
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-09-16 02:58:45 +0530

    Added comments

--- js/main.js
+++ js/main.js
@@ -12,10 +12,13 @@ const links = [
     label: "Week1 notes",
     url: "week1/index.html",
   },
-  {
+  /*{
     label: "Week2 notes",
     url: "week2/index.html",
   },
+  
+  //Uncomment when the second week is made available
+  */
 ];
 
 links.forEach((link) => {
