b28792bfcbba7714ed06d1feecfe237fa8506510
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-09-16 02:45:05 +0530

    Check new code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-const links = [
+/* const links = [
   {
     label: "Week1 notes",
     url: "week1/index.html",
   },
 ];
+*/
+
+const tableContents = document.querySelector("#table-contents");
+const links = [
+  {
+    label: "Week1 notes",
+    url: "Week1/index.html",
+  },
+  {
+    label: "Week2 notes",
+    url: "Week2/index.html",
+  },
+];
+
+links.forEach((link) => {
+  const list = document.createElement("li");
+  const url = document.createElement("a");
+  url.setAttribute("href", link.url);
+  url.textContent = link.label;
+  list.appendChild(url);
+  tableContents.appendChild(list);
+});
