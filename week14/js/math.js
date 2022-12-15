91df9cf1c511e89a6d3ba44b0a014e27c390f5cf
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2022-12-15 12:37:11 +0530

    rest code

--- week14/js/math.js
+++ week14/js/math.js
@@ -14,7 +14,7 @@ function solve(e, t, s, l) {
   switch (t) {
     case "+":
       let o = parseFloat(e) + parseFloat(s);
-      console.log(o),
+     // console.log(o),
         console.log("This is addition"),
         (document.getElementById("resultmath").innerText = l + "=" + o),
         (document.getElementById("resultmath").style.display = "block");
@@ -22,13 +22,19 @@ function solve(e, t, s, l) {
     case "-":
       let i = parseFloat(e) - parseFloat(s);
       console.log(i), console.log("This is subtraction");
+        (document.getElementById("resultmath").innerText = l + "=" + i),
+        (document.getElementById("resultmath").style.display = "block");
       break;
     case "*":
       let r = parseFloat(e) * parseFloat(s);
       console.log(r), console.log("This is multiplication");
+        (document.getElementById("resultmath").innerText = l + "=" + r),
+          (document.getElementById("resultmath").style.display = "block");
       break;
     case "/":
       let a = parseFloat(e) / parseFloat(s);
       console.log(a), console.log("This is division");
+        (document.getElementById("resultmath").innerText = l + "=" + a),
+          (document.getElementById("resultmath").style.display = "block");
   }
 }
